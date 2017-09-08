7a74e7dd1823b67994d8510300627e80a926487c
System <none>
2017-09-08 08:32:28 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,25 +12,37 @@ app.use(express.static('public'));
 
 
 app.get("/", function (request, response) {
-  response.redirect(`/${moment().format('unix')}`);
+  response.redirect(`/${moment().format('unix').slice(3)}`);
 });
 
 app.get("/:id", function (request, response) {
   var date = request.params.id;
   var properDate = '';
-  
-  var valid = (new Date(date)).getTime() > 0;
+  var valid = false;
   
   if (date > 0){
     if(date > 100000000000){
       date /= 1000,
-      properDate = moment.unix(date).format("MMMM, DD, YYYY");
-      response.render('index', {unix: date, natural: properDate, valid : valid});
-    }else{
-      properDate = moment.unix(date).format("MMMM, DD, YYYY");
-      response.render('index', {unix: date, natural: properDate, valid : valid});
+      valid = moment.unix(date).format('x') > 0;
+      if (valid === true){
+        properDate = moment.unix(date).format("MMMM, DD, YYYY");
+        date *= 1000;
+        response.render('index', {unix: date, natural: properDate, valid : valid});       
+      } else {
+          response.send("sorry");
+      }
+
+    } else {
+      valid = moment.unix(date).format('x') > 0;
+      if (valid == true){
+        properDate = moment.unix(date).format("MMMM, DD, YYYY");
+        response.render('index', {unix: date, natural: properDate, valid : valid});       
+      } else {
+          response.send("sorry");
+      }
     }
-  }else {
+  } else {
+      date = moment(date).format('x');
       properDate = moment(date).format("MMMM, DD, YYYY");   
       response.render('index', {natural: date, unix: properDate, valid : valid});
   }
